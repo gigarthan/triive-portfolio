9704d10ef864e58d2d12ed4b41371b0eebf759b5
gigarthan <gigajpx@gmail.com>
2020-01-10 09:38:39 +0530

    feat: initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `triive.agency`,
+    description: `Triive is a development agency focused to build applications with design in mind`,
+    author: `@gigarthan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,17 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Comfortaa`,
+          `source sans pro\:300,400,400i,700`, // you can also specify font weights and styles
+        ],
+        display: "swap",
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import "../styles/home.scss"
 
 const IndexPage = () => (
-  <Layout>
+  <>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+    <section className="home">
+      <section className="bg intro-bg">
+        <div className="card blur-background">
+          <h1 className="title">
+            <span className="title-sm">Welcome to</span> triive
+          </h1>
+        </div>
+      </section>
+      <section className="bg from-bg">
+        <div className="card blur-background">
+          <h1 className="title from-title">
+            We are a creative Agency from the land of Pearls (Sri Lanka)
+          </h1>
+        </div>
+      </section>
+      <section className="bg solve-bg">
+        <div className="card blur-background">
+          <h1 className="title from-title">
+            We focus solving problems in a pragmatic way to build your business
+            to the next level
+          </h1>
+        </div>
+      </section>
+      <section className="bg dev-bg">
+        <div className="card blur-background">
+          <h1 className="title from-title">We Develop</h1>
+        </div>
+      </section>
+      <section className="bg built-bg">
+        <div className="card blur-background">
+          <h1 className="title from-title">We have built</h1>
+        </div>
+      </section>
+      <section className="bg chat-bg">
+        <div className="card blur-background">
+          <h1 className="title from-title">Chat with us</h1>
+        </div>
+      </section>
+    </section>
+  </>
 )
 
 export default IndexPage
